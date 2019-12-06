60a5c8adb28b4aa6d6fd1be9cf26752f0189fa3d
mefiz <47503440+mefiz@users.noreply.github.com>
2019-12-07 00:02:40 +0500

    finished ui update. logic will be implemented next. Tasks and home ui left until the latest

--- src/main/java/com/untitled/controllers/MainController.java
+++ src/main/java/com/untitled/controllers/MainController.java
@@ -11,9 +11,12 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.effect.BlurType;
+import javafx.scene.effect.DropShadow;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 
 public class MainController {
@@ -26,7 +29,10 @@ public class MainController {
     
     @FXML
     private BorderPane borderPane;
-
+    
+    @FXML
+    private HBox topBar;
+    
     @FXML
     private JFXButton navButton;
     
@@ -41,6 +47,9 @@ public class MainController {
 
     @FXML
     private JFXButton salesButton;
+   
+    @FXML
+    private JFXButton salesHistoryButton;
 
     @FXML
     private JFXButton tasksButton;
@@ -85,73 +94,106 @@ public class MainController {
                 navIcon.setFill(Color.web("#7b9bbd"));
             }
             
-        });//end navButton.addEventHandler(MouseEvent.MOUSE_CLICKED, (e) ->{})
-        
-        //load the home view into the center of the border pane
-        homeButton.addEventHandler(MouseEvent.MOUSE_CLICKED, (e) ->{
-            try {
-                resetButtons();
-
-                FXMLLoader homeLoader = new FXMLLoader(getClass().getResource("/main/resources/com/untitled/view/Home.fxml"));
-                
-                AnchorPane home = homeLoader.load(); //load the home fxml and set it to the anchor pane
-                HomeController homeController = homeLoader.getController(); //set the controller
-                
-                borderPane.setCenter(home); //set it to center
-                
-                /*
-                disable the button to prevent the user from clicking the button
-                and having to perform the same operations again
-                */
-                homeButton.setDisable(true);
-            } catch (IOException ex) {
-                Logger.getLogger(MainController.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }); //end homeButton.addEventHandler(MouseEvent.MOUSE_CLICKED, (e) ->{})
-        
-        //load the sales view into the center of the border pane
-//        salesButton.addEventHandler(MouseEvent.MOUSE_CLICKED, (e) ->{
-//            resetButtons();
-//        });
-        
-        //load the parties view into the center of the border pane
-        partiesButton.addEventHandler(MouseEvent.MOUSE_CLICKED, (e) ->{
-            resetButtons(); //reset the buttons
-            //load the anchor pane and controller from the hashmaps
-            PartiesController partiesController = (PartiesController) viewControllers.get("Parties");
-            borderPane.setCenter(viewPanes.get("Parties")); //set it to center
-            /*
-            disable the button to prevent the user from clicking the button
-            and having to perform the same operations again
-            */
-            partiesButton.setDisable(true);
-        }); //end partiesButton.addEventHandler(MouseEvent.MOUSE_CLICKED, (e) ->{})
-        
-        //load the parties view into the center of the border pane
-        venuesButton.addEventHandler(MouseEvent.MOUSE_CLICKED, (e) ->{
-            resetButtons(); //reset the buttons
-            //load the anchor pane and controller from the hashmaps
-            VenuesController venuesController = (VenuesController) viewControllers.get("Venues");
-            borderPane.setCenter(viewPanes.get("Venues")); //set it to center
-            /*
-            disable the button to prevent the user from clicking the button
-            and having to perform the same operations again
-            */
-            venuesButton.setDisable(true);
-        }); //end venuesButton.addEventHandler(MouseEvent.MOUSE_CLICKED, (e) ->{})
-        
-        //load the customers view into the center of the pane
-        customersButton.addEventHandler(MouseEvent.MOUSE_CLICKED, (e) ->{
-            resetButtons(); //reset the buttons
-            //load the anchor pane and controller from the hashmaps
-            CustomersController customersController = (CustomersController) viewControllers.get("Customers");
-            borderPane.setCenter(viewPanes.get("Parties")); //set it to center
-            /*
-            disable the button to prevent the user from clicking the button
-            and having to perform the same operations again
-            */
-            customersButton.setDisable(true);
         }); //end customersButton.addEventHandler(MouseEvent.MOUSE_CLICKED, (e) ->{})
+        
+        homeButton.setOnAction((event) -> {
+            resetButtons();
+            
+            //load from hashmap
+            HomeController homeController = (HomeController) viewControllers.get("Home");
+            borderPane.setCenter(viewPanes.get("Home"));
+            
+            //disable the buttons
+            homeButton.setDisable(true);
+        });
+        
+        salesButton.setOnAction((event) -> {
+            resetButtons();
+            
+            //load from hashmap
+            SaleController saleController = (SaleController) viewControllers.get("Sale");
+            borderPane.setCenter(viewPanes.get("Sale"));
+            
+            //disable the buttons
+            salesButton.setDisable(true);
+        });
+        
+        salesHistoryButton.setOnAction((event) -> {
+            resetButtons();
+            
+            //load from hashmap
+            SalesHistoryController saleHistoryController = (SalesHistoryController) viewControllers.get("SalesHistory");
+            borderPane.setCenter(viewPanes.get("SalesHistory"));
+            
+            //disable the buttons
+            salesHistoryButton.setDisable(true);
+        });
+        
+        tasksButton.setOnAction((event) -> {
+            resetButtons();
+            
+            //load from hashmap
+            TasksController tasksController = (TasksController) viewControllers.get("Tasks");
+            borderPane.setCenter(viewPanes.get("Tasks"));
+            
+            //disable the buttons
+            tasksButton.setDisable(true);
+        });
+        
+        partiesButton.setOnAction((event) -> {
+            resetButtons();
+            
+            //load from hashmap
+            PartiesController partiesController = (PartiesController) viewControllers.get("Parties");
+            borderPane.setCenter(viewPanes.get("Parties"));
+            
+            //disable the buttons
+            partiesButton.setDisable(true);
+        });
+        
+        venuesButton.setOnAction((event) -> {
+            resetButtons();
+            
+            //load from hashmap
+            VenuesController venuesController = (VenuesController) viewControllers.get("Venues");
+            borderPane.setCenter(viewPanes.get("Venues"));
+            
+            //disable the buttons
+            venuesButton.setDisable(true);
+        });
+        
+        addonsButton.setOnAction((event) -> {
+            resetButtons();
+            
+            //load from hashmap
+            AddonsController addonsController = (AddonsController) viewControllers.get("Addons");
+            borderPane.setCenter(viewPanes.get("Addons"));
+            
+            //disable the buttons
+            addonsButton.setDisable(true);
+        });
+        
+        customersButton.setOnAction((event) -> {
+            resetButtons();
+            
+            //load from hashmap
+            CustomersController customersController = (CustomersController) viewControllers.get("Customers");
+            borderPane.setCenter(viewPanes.get("Customers"));
+            
+            //disable the buttons
+            customersButton.setDisable(true);
+        });
+        
+        userSettingsButton.setOnAction((event) -> {
+            resetButtons();
+            
+            //load from hashmap
+            UserSettingsController userSettingsController = (UserSettingsController) viewControllers.get("Users");
+            borderPane.setCenter(viewPanes.get("Users"));
+            
+            //disable the buttons
+            userSettingsButton.setDisable(true);
+        });
     }
     
     //set a disabled button to enabled
@@ -170,6 +212,13 @@ public class MainController {
         */
         if(salesButton.isDisabled() == true){
             salesButton.setDisable(false);
+        }
+        
+        /*
+        Sales History Button
+        */
+        if(salesHistoryButton.isDisabled() == true){
+            salesHistoryButton.setDisable(false);
         }
         
         /*
@@ -215,6 +264,8 @@ public class MainController {
         }
     }//end private void resetButtons(){}
     
+    //reapply css for the topbar and the nav bar
+    
     private void initializeView(){
         //create a new thread to load the scenes
         new Thread(() -> {
@@ -224,16 +275,42 @@ public class MainController {
                 /*
                 Home
                 */
-                
+                FXMLLoader homeLoader = new FXMLLoader(getClass().getResource("/main/resources/com/untitled/view/Home.fxml"));
+                AnchorPane homePane = homeLoader.load();
+                HomeController homeController = homeLoader.getController();
+                //add to the hashmaps
+                viewPanes.put("Home", homePane);
+                viewControllers.put("Home", homeController);
                 
                 /*
                 Sales
                 */
+                FXMLLoader saleLoader = new FXMLLoader(getClass().getResource("/main/resources/com/untitled/view/Sale.fxml"));
+                AnchorPane salePane = saleLoader.load();
+                SaleController saleController = saleLoader.getController();
+                //add to the hashmaps
+                viewPanes.put("Sale", salePane);
+                viewControllers.put("Sale", saleController);
+                
+                /*
+                Sales History
+                */
+                FXMLLoader salesHistoryLoader = new FXMLLoader(getClass().getResource("/main/resources/com/untitled/view/SalesHistory.fxml"));
+                AnchorPane salesHistoryPane = salesHistoryLoader.load();
+                SalesHistoryController salesHistoryController = salesHistoryLoader.getController();
+                //add to the hashmaps
+                viewPanes.put("SalesHistory", salesHistoryPane);
+                viewControllers.put("SalesHistory", salesHistoryController);
                 
                 /*
                 Tasks
                 */
-                
+                FXMLLoader tasksLoader = new FXMLLoader(getClass().getResource("/main/resources/com/untitled/view/Tasks.fxml"));
+                AnchorPane tasksPane = tasksLoader.load();
+                TasksController tasksController = tasksLoader.getController();
+                //add to the hashmaps
+                viewPanes.put("Tasks", tasksPane);
+                viewControllers.put("Tasks", tasksController);
                 
                 /*
                 Parties
@@ -241,7 +318,7 @@ public class MainController {
                 FXMLLoader partiesLoader = new FXMLLoader(getClass().getResource("/main/resources/com/untitled/view/Parties.fxml"));
                 AnchorPane partiesPane = partiesLoader.load();
                 PartiesController partiesController = partiesLoader.getController();
-                //add the controllers into the hashmaps
+                //add to the hashmaps
                 viewPanes.put("Parties", partiesPane);
                 viewControllers.put("Parties", partiesController);
                 
@@ -251,13 +328,19 @@ public class MainController {
                 FXMLLoader venuesLoader = new FXMLLoader(getClass().getResource("/main/resources/com/untitled/view/Venues.fxml"));
                 AnchorPane venuesPane = venuesLoader.load();
                 VenuesController venuesController = venuesLoader.getController();
-                //add the controllers into the hashmaps
+                //add to the hashmaps
                 viewPanes.put("Venues", venuesPane);
                 viewControllers.put("Venues", venuesController);
                 
                 /*
                     Add-ons and Catering
                 */
+                FXMLLoader addonsLoader = new FXMLLoader(getClass().getResource("/main/resources/com/untitled/view/Addons.fxml"));
+                AnchorPane addonsPane = addonsLoader.load();
+                AddonsController addonsController = addonsLoader.getController();
+                //add to the hashmap
+                viewPanes.put("Addons", addonsPane);
+                viewControllers.put("Addons", addonsController);
                 
                 /*
                     Customers
@@ -265,13 +348,20 @@ public class MainController {
                 FXMLLoader customersLoader = new FXMLLoader(getClass().getResource("/main/resources/com/untitled/view/Customers.fxml"));
                 AnchorPane customersPane = customersLoader.load();
                 CustomersController customersController = customersLoader.getController();
-                //add the controllers into the hashmaps
+                //add to the hashmaps
                 viewPanes.put("Customers", customersPane);
                 viewControllers.put("Customers", customersController);
                 
                 /*
                     Users
                 */
+                FXMLLoader usersLoader = new FXMLLoader(getClass().getResource("/main/resources/com/untitled/view/UserSettings.fxml"));
+                AnchorPane userSettingsPane = usersLoader.load();
+                UserSettingsController userSettingsController = usersLoader.getController();
+                //add to the hashmaps
+                viewPanes.put("Users", userSettingsPane);
+                viewControllers.put("Users", userSettingsController);
+                
             } catch (IOException ex) {
                 Logger.getLogger(MainController.class.getName()).log(Level.SEVERE, null, ex);
             }
